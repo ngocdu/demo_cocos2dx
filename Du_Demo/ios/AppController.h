08fc2dda79491f9c787adda7b09d2c1569d8f5b0
AceMNT <nghiem.tuan.minh@framgia.com>
2013-06-05 09:08:13 +0700

    first commit

---
+++ Du_Demo/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Du_DemoAppController.h
+//  Du_Demo
+//
+//  Created by MinhNT on 13/06/04.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Du_Demo/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Du_DemoAppController.h
+//  Du_Demo
+//
+//  Created by MinhNT on 13/06/04.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
